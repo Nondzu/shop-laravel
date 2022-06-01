ec0d61683b5d9a21d88b2ba5cc21bf55ac38c5d9
Nondzu <kamil.nonckiewicz@gmail.com>
2022-06-01 17:29:22 +0200

    email

--- routes/web.php
+++ routes/web.php
@@ -19,22 +19,26 @@ use App\Http\Controllers\HelloWordlController;
 */
 //home
 Route::get('/', [WelcomeController::class, 'index']);
-
-// products
-Route::get('/products', [ProductController::class, 'index'])->name('products.index')->middleware('auth');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create')->middleware('auth');
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show')->middleware('auth');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store')->middleware('auth');
-Route::get('/products/edit/{product}', [ProductController::class, 'edit'])->name('products.edit')->middleware('auth');
-Route::post('/products/{product}', [ProductController::class, 'update'])->name('products.update')->middleware('auth');
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy')->middleware('auth');
-
-// users
-Route::get('/users/list', [UserController::class, 'index'])->middleware('auth');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('auth');
-
 Route::get('/hello', [HelloWordlController::class, 'show']);
 
-Auth::routes();
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('products', ProductController::class);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    // users
+    Route::get('/users/list', [UserController::class, 'index'])->middleware('auth');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('auth');
+
+    //test
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+// products
+// Route::get('/products', [ProductController::class, 'index'])->name('products.index')->middleware('auth');
+// Route::get('/products/create', [ProductController::class, 'create'])->name('products.create')->middleware('auth');
+// Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show')->middleware('auth');
+// Route::post('/products', [ProductController::class, 'store'])->name('products.store')->middleware('auth');
+// Route::get('/products/edit/{product}', [ProductController::class, 'edit'])->name('products.edit')->middleware('auth');
+// Route::post('/products/{product}', [ProductController::class, 'update'])->name('products.update')->middleware('auth');
+// Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy')->middleware('auth');
+
+Auth::routes(['verify' => true]);
