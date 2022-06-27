4653709114b9ec98fc3b34b68901ee7b83439358
Nondzu <kamil.nonckiewicz@gmail.com>
2022-06-27 13:45:26 +0200

    swagger docs install

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * @OA\OpenApi(
+ *  @OA\Info(
+ *      title="Returns Services API",
+ *      version="1.0.0",
+ *      description="API documentation for Returns Service App",
+ *      @OA\Contact(
+ *          email="sushil@stepfront.com"
+ *      )
+ *  ),
+ *  @OA\Server(
+ *      description="Returns App API",
+ *      url="http://localhost/"
+ *  ),
+ *  @OA\PathItem(
+ *      path="/"
+ *  )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
